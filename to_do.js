7f92d915d0277eb21133e6e5ed1b25fc592e8d04
Upshur06 <mupshur2005@gmail.com>
2019-10-12 15:38:40 -0400

    most done

--- to_do.js
+++ to_do.js
@@ -4,20 +4,12 @@ var buttonP = document.getElementById("btn");
 
 function printList(){
   var newDiv = document.createElement("div");
-  newDiv.style.border = " 3px solid grey";
-  newDiv.style.margin = "0% 0% 1% 10%";
-  newDiv.style.padding = "0% 0% 0% 20%";
-  newDiv.style.width = "70%";
-  newDiv.style.height = "50px";
+  newDiv.className = "textList";
   newDiv.innerHTML = inputM.value;
-  newDiv.style.fontSize = "30px";
   document.body.appendChild(newDiv);
 
   var btn = document.createElement("button");
-  btn.innerHTML = "x";
-  btn.style.border = " 1px solid black";
-  btn.style.margin = "0% 0% 1% 20%";
-  btn.style.width = "10%";
+  btn.className ="glyphicon glyphicon-remove"
   newDiv.appendChild(btn);
 
   var listComplete = newDiv.addEventListener("click", function(){
@@ -38,17 +30,10 @@ function printList(){
 
 
 
-// function showcase(){
-//   printList().addEventListener("click", function(){
-//     this.style.display = "none";
-//   })
-// }
-
 function mark(){
   buttonP.addEventListener("click", function(){
-    // listSpace.innerHTML = inputM.value;
     printList();
-    inputM.value = inputM.placeholder;
+    inputM.value = info.placeholder;
   });
 }
 
